450edda1de5fbf750eebae9d8f9e4cd0faa0de5b
Axel Larsson <65452706+axla-io@users.noreply.github.com>
2023-04-02 17:01:16 +0200

    Started steady state work

--- src/include_lib.jl
+++ src/include_lib.jl
@@ -3,6 +3,7 @@ using Graphs
 using StaticArrays
 using LinearAlgebra
 using NBodySimulator
+using SteadyStateDiffEq
 using StaticGraphs
 using ForwardDiff
 
@@ -11,11 +12,11 @@ using JET
 
 include("elem.jl")
 include("node.jl")
+include("analysis/system.jl")
 include("simulation.jl")
 include("analysis/loads.jl")
 include("analysis/rotations.jl")
 include("analysis/constraints.jl")
 include("analysis/3dof_acceleration.jl")
 include("analysis/6dof_acceleration.jl")
-include("analysis/system.jl")
 include("optimization/load_finding.jl")
