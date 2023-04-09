5e0f4ebdf8d77f6080470a0a53c7bc0cd8990919
Axel Larsson <65452706+axla-io@users.noreply.github.com>
2023-04-09 21:15:27 +0200

    made tests faster

--- test/simple_bar_simulation.jl
+++ test/simple_bar_simulation.jl
@@ -5,11 +5,11 @@ using StaticGraphs
 using DiffEqCallbacks
 using NBodySimulator
 
-using Plots, GraphRecipes
+#using Plots, GraphRecipes
 using Test
 
 @testset "Bar simulation" begin
-    
+
     # Define a simple graph system
     n_elem = 17
     n_pt = n_elem + 1
@@ -42,22 +42,24 @@ using Test
 
     # Plot final state
     u_final = get_state(sol.u[end], u_len, simulation)
-    plot(u_final[1, :], u_final[3, :])
+    #plot(u_final[1, :], u_final[3, :])
 
-    u_final_true = [0.0 0.9999178077243016 1.99986643409378 2.9998417722609956 3.999839714390765 4.999856151811957 5.999886975169358 6.9999280745756245 7.999975339763273 9.000024660236727 10.000071925424376 11.000113024830641 12.000143848188042 13.000160285609235 14.000158227739007 15.00013356590622 16.000082192275695 17.0; 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0; 0.0 -0.016218327579968574 -0.030409801596914338 -0.042574247125090364 -0.052711514208112986 -0.06082147786820732 -0.06690403811390315 -0.07095911994620795 -0.07298667336321966 -0.07298667336321886 -0.07095911994620602 -0.06690403811389987 -0.06082147786820441 -0.052711514208110134 -0.04257424712508335 -0.03040980159690485 -0.016218327579965747 0.0]
+    u_final_true = [0.0 0.9999178077243016 1.99986643409378 2.9998417722609956 3.999839714390765 4.999856151811957 5.999886975169358 6.9999280745756245 7.999975339763273 9.000024660236727 10.000071925424376 11.000113024830641 12.000143848188042 13.000160285609235 14.000158227739007 15.00013356590622 16.000082192275695 17.0;
+                    0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+                    0.0 -0.016218327579968574 -0.030409801596914338 -0.042574247125090364 -0.052711514208112986 -0.06082147786820732 -0.06690403811390315 -0.07095911994620795 -0.07298667336321966 -0.07298667336321886 -0.07095911994620602 -0.06690403811389987 -0.06082147786820441 -0.052711514208110134 -0.04257424712508335 -0.03040980159690485 -0.016218327579965747 0.0]
 
     @test isapprox(sum(abs, u_final .- u_final_true), 0.0, atol = 1e-7)
+    #= 
+        # Select frames for animation
+        itt = generate_range(100, 1, length(sol.u))
+        u_red = sol.u[itt]
 
-    # Select frames for animation
-    itt = generate_range(100, 1, length(sol.u))
-    u_red = sol.u[itt]
+        # Loop over the time values and create a plot for each frame
+        anim = @animate for i in axes(u_red, 1)
+            u_final = get_state(u_red[i], u_len, simulation)
+            plot(u_final[1, :], u_final[3, :])
+        end
 
-    # Loop over the time values and create a plot for each frame
-    anim = @animate for i in axes(u_red, 1)
-        u_final = get_state(u_red[i], u_len, simulation)
-        plot(u_final[1, :], u_final[3, :])
-    end
-
-    # Save the frames as a gif
-    gif(anim, fps = 20)
+        # Save the frames as a gif
+        gif(anim, fps = 20) =#
 end
--- test/simple_beam_simulation.jl
+++ test/simple_beam_simulation.jl
@@ -5,7 +5,7 @@ using StaticGraphs
 using DiffEqCallbacks
 using NBodySimulator
 
-using Plots, GraphRecipes
+#using Plots, GraphRecipes
 using Test
 
 @testset "Beam simulation" begin
@@ -43,14 +43,14 @@ using Test
 
     # Plot final state
     u_final = get_state(sol.u[end], u_len, simulation)
-    plot(u_final[1, :], u_final[3, :])
+    #plot(u_final[1, :], u_final[3, :])
 
     u_final_true = [0.0 0.9986722089024265 1.9977858647654367 2.997273043411182 3.997143682802792 4.997451957090221 5.998094369849439 6.998858956587347 7.999628750605406 9.000371249394592 10.00114104341265 11.001905630150565 12.00254804290978 13.002856317197205 14.00272695658882 15.002214135234565 16.001327791097573 17.0;
                     0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
                     0.0 -0.061571676604151505 -0.11539880745740831 -0.1612191993541145 -0.199801504907901 -0.23036953682824707 -0.25354819106840504 -0.26928831873876025 -0.27716124078038057 -0.2771612407803893 -0.2692883187387831 -0.2535481910684114 -0.2303695368282427 -0.1998015049078976 -0.16121919935412593 -0.11539880745739693 -0.06157167660414585 0.0]
 
     @test isapprox(sum(abs, u_final .- u_final_true), 0.0, atol = 1e-7)
-
+#= 
     # Select frames for animation
     itt = generate_range(100, 1, length(sol.u))
     u_red = sol.u[itt]
@@ -62,5 +62,5 @@ using Test
     end
 
     # Save the frames as a gif
-    gif(anim, fps = 20)
+    gif(anim, fps = 20) =#
 end
