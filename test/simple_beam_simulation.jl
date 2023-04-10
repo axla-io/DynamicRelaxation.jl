fff4ae88a476f3051b840564858c74a997d4646b
Axel Larsson <65452706+axla-io@users.noreply.github.com>
2023-04-10 19:54:19 +0200

    Remade test for beam

--- test/simple_beam_simulation.jl
+++ test/simple_beam_simulation.jl
@@ -5,7 +5,7 @@ using StaticGraphs
 using DiffEqCallbacks
 using NBodySimulator
 
-#using Plots, GraphRecipes
+using Plots, GraphRecipes
 using Test
 
 @testset "Beam simulation" begin
@@ -33,34 +33,37 @@ using Test
     (u0, v0, n, u_len, v_len) = get_u0(simulation)
     (dx_ids, dr_ids, v_ids, ω_ids) = get_vel_ids(u_len, v_len, system)
     v_decay!(integrator) = velocitydecay!(integrator, vcat(v_ids, ω_ids), c)
-    cb1 = PeriodicCallback(v_decay!, 3 * dt; initial_affect = true)
+    cb1 = PeriodicCallback(v_decay!, 2 * dt; initial_affect = true)
 
     # Set algorithm for solver
     alg = RK4()
 
     # Solve problem
-    @time sol = solve(prob, alg, dt = simulation.dt, maxiters = maxiters, callback = cb1);
+    @time sol = solve(prob, alg, dt = simulation.dt, maxiters = maxiters, callback = cb1)
 
     # Plot final state
     u_final = get_state(sol.u[end], u_len, simulation)
-    #plot(u_final[1, :], u_final[3, :])
+    #=
+    plot(u_final[1, :], u_final[3, :])
+    =#
 
-    u_final_true = [0.0 0.9986722089024265 1.9977858647654367 2.997273043411182 3.997143682802792 4.997451957090221 5.998094369849439 6.998858956587347 7.999628750605406 9.000371249394592 10.00114104341265 11.001905630150565 12.00254804290978 13.002856317197205 14.00272695658882 15.002214135234565 16.001327791097573 17.0;
+    u_final_true = [0.0 0.9985840905257405 1.9976994903128098 2.997274873545508 3.997239315074668 4.9975225625937725 5.9980536618600535 6.998761291272035 7.999575190903782 9.000424809096222 10.001238708727966 11.001946338139948 12.002477437406226 13.002760684925333 14.002725126454491 15.002300509687192 16.00141590947426 17.0;
                     0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
-                    0.0 -0.061571676604151505 -0.11539880745740831 -0.1612191993541145 -0.199801504907901 -0.23036953682824707 -0.25354819106840504 -0.26928831873876025 -0.27716124078038057 -0.2771612407803893 -0.2692883187387831 -0.2535481910684114 -0.2303695368282427 -0.1998015049078976 -0.16121919935412593 -0.11539880745739693 -0.06157167660414585 0.0]
+                    0.0 -0.06146492984809863 -0.1152539050247191 -0.16136341273093313 -0.19979091083461925 -0.23053544160821957 -0.25359487860921576 -0.26896819122986326 -0.27665478826820794 -0.27665478826819967 -0.26896819122984394 -0.25359487860923163 -0.23053544160822184 -0.19979091083461728 -0.16136341273093896 -0.11525390502471794 -0.06146492984809655 0.0]
 
     @test isapprox(sum(abs, u_final .- u_final_true), 0.0, atol = 1e-7)
-#= 
-    # Select frames for animation
-    itt = generate_range(100, 1, length(sol.u))
-    u_red = sol.u[itt]
+    #= 
+        # Select frames for animation
+        itt = generate_range(100, 1, length(sol.u))
+        u_red = sol.u[itt]
 
-    # Loop over the time values and create a plot for each frame
-    anim = @animate for i in axes(u_red, 1)
-        u_final = get_state(u_red[i], u_len, simulation)
-        plot(u_final[1, :], u_final[3, :])
-    end
+        # Loop over the time values and create a plot for each frame
+        anim = @animate for i in axes(u_red, 1)
+            u_final = get_state(u_red[i], u_len, simulation)
+            plot(u_final[1, :], u_final[3, :])
+        end
 
-    # Save the frames as a gif
-    gif(anim, fps = 20) =#
+        # Save the frames as a gif
+        gif(anim, fps = 20) 
+        =#
 end
