08bba25f9068edb78edd5fc5514083867d1b7327
Axel Larsson <65452706+axla-io@users.noreply.github.com>
2023-04-10 17:32:37 +0200

    Made it work, but still bugs

--- examples/load_find_beam.jl
+++ examples/load_find_beam.jl
@@ -10,7 +10,7 @@ using Graphs
 using StaticGraphs
 using DiffEqCallbacks
 using NBodySimulator
-using DiffEqFlux
+#using DiffEqFlux
 
 # GENERATE DATA
 # -----------------------------------------------------
@@ -25,7 +25,7 @@ system = default_system(graph, Node6DOF, :catenary, n_pt)
 ext_f = uniform_load(Pz(-10_000, system), system)
 
 # Set parameters
-maxiters = 500
+maxiters = 1000
 dt = 0.01
 tspan = (0.0, 10.0)
 
@@ -37,7 +37,7 @@ prob = ODEProblem(simulation, ext_f)
 c = 0.7
 (_u0, _v0, n, u_len, v_len) = get_u0(simulation)
 (dx_ids, dr_ids, v_ids, ω_ids) = get_vel_ids(u_len, v_len, system)
-v_decay!(integrator) = velocitydecay!(integrator, v_ids, c)
+v_decay!(integrator) = velocitydecay!(integrator, vcat(v_ids, ω_ids), c)
 
 cb1 = PeriodicCallback(v_decay!, 1 * dt; initial_affect = true)
 
@@ -46,7 +46,7 @@ alg = RK4()
 
 # Solve problem, corresponding to p = 1.0
 p_true = [1.0]
-@time sol = solve(prob, p = p_true, alg, dt = simulation.dt, maxiters = maxiters);#, callback=cb1);
+@time sol = solve(prob, p = p_true, alg, dt = simulation.dt, maxiters = maxiters, callback=cb1);
 # Extract final state
 u_final = get_state(sol.u[end], u_len, simulation)
 
@@ -58,29 +58,26 @@ u0 = sol.u[1]
 
 # Create a solution (prediction) for a given starting point u0 and set of
 # parameters p
-#= function predict(p)
-    return solve(prob, alg, p = p, dt = simulation.dt, maxiters = maxiters, callback = cb)
-end =#
+function predict(p)
+    return get_state(solve(prob, alg, p = p, dt = simulation.dt, maxiters = maxiters, callback = cb1, verbose=false).u[end], u_len, simulation)
+end 
 
 # Create loss function
 function l2loss(p)
-    prediction = solve(prob, alg, p = p, dt = simulation.dt, maxiters = maxiters)#, callback = cb1)
-    u_pred = get_state(prediction.u[end], u_len, simulation)
+    u_pred = predict(p)
     loss = sum(abs2, u_pred .- u_final)
-    return loss, prediction
+    return loss, u_pred
 end
 
 function l1loss(p)
-    prediction = solve(prob, alg, p = p, dt = simulation.dt, maxiters = maxiters)#, callback = cb1)
-    u_pred = get_state(prediction.u[end], u_len, simulation)
+    u_pred = predict(p)
     loss = sum(abs, u_pred .- u_final)
-    return loss, prediction
+    return loss, u_pred
 end
 
 # Initial guess of p
 p0 = [0.3]
-sol_pred_init = solve(prob, alg, p = p0, dt = simulation.dt, maxiters = maxiters)#,
-#callback = cb1);
+sol_pred_init = solve(prob, alg, p = p0, dt = simulation.dt, maxiters = maxiters, callback = cb1);
 
 l2loss(p0)
 # Plot final state
@@ -100,7 +97,7 @@ callback = function (p, l, prediction)
     iter += 1
 
     display(l)
-    u_pred = get_state(prediction.u[end], u_len, simulation)
+    u_pred = prediction #get_state(prediction.u[end], u_len, simulation)
 
     # using `remake` to re-create our `prob` with current parameters `p`
     #sol_pred = solve(remake(prob, p=p), alg, dt=simulation.dt, maxiters=maxiters, callback=cb)
@@ -110,7 +107,7 @@ callback = function (p, l, prediction)
     plot!(plt, u_pred_init[1, :], u_pred_init[3, :], lw = 1.5, label = "Initial Prediction")
     plot!(plt, u_pred[1, :], u_pred[3, :], lw = 1.5, label = "Prediction, iter. $iter")
     p_pred = round(p[1], digits = 4)
-    plot!(plt, ylims = (-0.3, 0.0),
+    plot!(plt, ylims = (-0.4, 0.0),
           title = "\nLoad Finding, \$p_{\\rm{true}}\$ = 1.0, \$p_{\\rm{pred}}\$ = $p_pred")
 
     push!(list_plots, plt)
@@ -142,8 +139,8 @@ function axl_train(loss, θ, opt = OptimizationPolyalgorithms.PolyOpt(), adtype 
     Optimization.solve(optprob, opt, args...; maxiters, callback = callback, kwargs...)
 end
 
-result_ode1 = axl_train(l2loss, p0,
-OptimizationOptimisers.Adam(0.03), maxiters=20)
+@time result_ode1 = axl_train(l2loss, p0,
+BFGS(initial_stepnorm = 0.0001), maxiters=3, callback=callback)
 
 #= result_ode1 = DiffEqFlux.sciml_train(l2loss, p0,
 OptimizationOptimisers.Adam(0.03), maxiters=20) =#
@@ -172,12 +169,12 @@ result_ode2 = Optimization.solve(optprob2, BFGS(initial_stepnorm = 0.0001),
 p_1 = result_ode1.minimizer
 
 # Solve problem
-@time sol_pred = solve(remake(prob, p = p_1), alg, dt = simulation.dt, maxiters = maxiters,
-                       callback = cb);
+@time sol_pred = solve(remake(prob, p = p_1), alg, dt = simulation.dt, maxiters = maxiters,)
+                       #callback = cb);
 
 # Plot final state
 u_pred = get_state(sol_pred.u[end], u_len, simulation)
 plot(u_final[1, :], u_final[3, :], label = "Ground Truth")
 plot!(u_pred[1, :], u_pred[3, :], label = "Prediction")
 
-animate(list_plots, "load_finding.gif", fps = 2)
+animate(list_plots, "load_finding_bfgs.mp4", fps = 2)
